756c7c758418bd1b34f2310ec7cd089845a4fc5d
trevoryee <trevor_yee@icloud.com>
2022-10-05 14:23:28 -0500

    Updating h04_pets. Adding while and for loop versions.

--- Application Development/h04_pets_while.py
+++ Application Development/h04_pets_while.py
@@ -1,13 +1,12 @@
-# h04_pets_for.py
-# A program to remove rats, using a 'for' loop
+# h04_pets_while.py
+# A program to remove rats using a 'while' loop
 # created by Trevor Yee, 10/3/2022
 
 pets= ['dog', 'rat', 'koala','cat','rat','panda','goldfish','rat','sloth','rat','rabbit','fish','rat','dolphin']
 print("Pets (With rat:"+ str(pets).title())
 
-for i in pets [:]:
-    if i == "rat":
-        pets.remove("rat")
+while 'rat' in pets :
+    pets.remove("rat")
 
 
 print("Pets (Without rat):" + str(pets).title())
