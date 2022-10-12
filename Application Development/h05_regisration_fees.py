014294d5c05ec22a7b894c8492094e9f0a17dbc5
trevoryee <trevor_yee@icloud.com>
2022-10-12 12:37:32 -0500

    Update reg fees to have logic

--- Application Development/h05_regisration_fees.py
+++ Application Development/h05_regisration_fees.py
@@ -1,13 +1,35 @@
 #h05_regisration_fees.py
 
-vehicle= input("Please input the type of vehicle you have (Truck or Car): ")
-weight= float(input("Please input the weight of your vehicle: "))
+def vehinfo():
+    global vehicle, weightstr
+    vehicle= input("Please input the type of vehicle you have (Truck or Car): ")
+    weightstr= input("Please input the weight of your vehicle (Must be a number): ")
 
-if vehicle.casefold() == 'car' and weight < 3000:
-    print('Your vehicle is a car weighing', weight, 'and the registration fee is 125.00')
-elif vehicle.casefold() == 'car' and weight >= 3000:
-    print('Your vehicle is a car weighing', weight, 'and the registration fee is 200.00')
-elif vehicle.casefold() == 'car' and weight < 4000:
-    print('Your vehicle is a truck weighing', weight, 'and the registration fee is 250.00')
-elif vehicle.casefold() == 'car' and weight >= 4000:
-    print('Your vehicle is a truck weighing', weight, 'and the registration fee is 350.00')
+def checkinfo():
+    global isnumber
+    try:
+        float(weightstr)
+        isnumber= 1
+    except ValueError:
+        print(weightstr, 'is not a number')
+        isnumber= 0
+
+def calc():
+    if vehicle.casefold() == 'car' and float(weightstr) < 3000:
+        print('Your vehicle is a car weighing', weightstr, 'and the registration fee is 125.00')
+    elif vehicle.casefold() == 'car' and float(weightstr) >= 3000:
+        print('Your vehicle is a car weighing', weightstr, 'and the registration fee is 200.00')
+    elif vehicle.casefold() == 'car' and float(weightstr) < 4000:
+        print('Your vehicle is a truck weighing', weightstr, 'and the registration fee is 250.00')
+    elif vehicle.casefold() == 'car' and float(weightstr) >= 4000:
+        print('Your vehicle is a truck weighing', weightstr, 'and the registration fee is 350.00')
+
+
+vehinfo()
+checkinfo()
+while isnumber == 0:
+    print("You entered invalid information.")
+    vehinfo()
+    checkinfo()
+if isnumber == 1:
+    calc()
