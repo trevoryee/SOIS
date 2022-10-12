dc49942877446ece1a44f33dca3565638e99c41c
trevoryee <trevor_yee@icloud.com>
2022-10-12 12:01:48 -0500

    Initial version of registration fee app

--- Application Development/h05_regisration_fees.py
+++ Application Development/h05_regisration_fees.py
@@ -1,2 +1,13 @@
 #h05_regisration_fees.py
 
+vehicle= input("Please input the type of vehicle you have (Truck or Car): ")
+weight= float(input("Please input the weight of your vehicle: "))
+
+if vehicle.casefold() == 'car' and weight < 3000:
+    print('Your vehicle is a car weighing', weight, 'and the registration fee is 125.00')
+elif vehicle.casefold() == 'car' and weight >= 3000:
+    print('Your vehicle is a car weighing', weight, 'and the registration fee is 200.00')
+elif vehicle.casefold() == 'car' and weight < 4000:
+    print('Your vehicle is a truck weighing', weight, 'and the registration fee is 250.00')
+elif vehicle.casefold() == 'car' and weight >= 4000:
+    print('Your vehicle is a truck weighing', weight, 'and the registration fee is 350.00')
