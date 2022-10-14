62ff864968bb099430a1f55c4e2814899be772a5
trevoryee <trevor_yee@icloud.com>
2022-10-13 19:11:25 -0500

    Added checks if input is valid.

--- Application Development/h05_regisration_fees.py
+++ Application Development/h05_regisration_fees.py
@@ -5,32 +5,41 @@ def vehinfo():
     vehicle= input("Please input the type of vehicle you have (Truck or Car): ")
     weightstr= input("Please input the weight of your vehicle (Must be a number): ")
 
-def checkinfo():
+def checknum():
     global isnumber
     try:
         float(weightstr)
         isnumber= 1
     except ValueError:
-        print(weightstr, 'is not a number')
+        print(str(weightstr).title(), 'is not a number')
         isnumber= 0
+
+def checkveh():
+    global isvehicle
+    if vehicle.casefold() in ('car','truck'):
+        isvehicle= 1
+    else:
+        print('Input not car or truck')
+        isvehicle= 0
 
 def calc():
     if vehicle.casefold() == 'car' and float(weightstr) < 3000:
         print('Your vehicle is a car weighing', weightstr, 'and the registration fee is 125.00')
     elif vehicle.casefold() == 'car' and float(weightstr) >= 3000:
         print('Your vehicle is a car weighing', weightstr, 'and the registration fee is 200.00')
-    elif vehicle.casefold() == 'car' and float(weightstr) < 4000:
+    elif vehicle.casefold() == 'truck' and float(weightstr) < 4000:
         print('Your vehicle is a truck weighing', weightstr, 'and the registration fee is 250.00')
-    elif vehicle.casefold() == 'car' and float(weightstr) >= 4000:
+    elif vehicle.casefold() == 'truck' and float(weightstr) >= 4000:
         print('Your vehicle is a truck weighing', weightstr, 'and the registration fee is 350.00')
 
 
 vehinfo()
-checkinfo()
-
-while isnumber == 0:
+checknum()
+checkveh()
+while (isnumber == 0) or (isvehicle == 0):
     print("You entered invalid information.")
     vehinfo()
-    checkinfo()
-if isnumber == 1:
+    checknum()
+    checkveh()
+if (isnumber == 1) and (isvehicle) == 1:
     calc()
