660f053078e8e8ac5c3fc63ae1b23db68a720ff2
Trevor Yee <trevor.yeee@gmail.com>
2022-11-02 12:47:26 -0500

    PHP

---
+++ Web Design/yeet.uwm.sois.com/forms/action_page_2.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome, your name and username are: <?php echo$_POST["textinput1"]; ?> and <?php echo$_POST["textinput2"]; ?><br>
+<?php echo$_POST["radio"]; ?>
+<?php echo$_POST["transport"]; ?>
+</body>
+</html> 
